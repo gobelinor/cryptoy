c4a852466bace55b5f8c6745b93839925d4badee
Juillard Thibault <thibault.juillard@ecole2600.com>
2024-08-03 12:51:52 +0200

    emballé cest pesé

--- src/cryptoy/diffie_hellman.py
+++ src/cryptoy/diffie_hellman.py
@@ -13,10 +13,13 @@ def keygen(prime_number: int, generator: int) -> dict[str, int]:
     # 1. Tire aléatoirement un nombre secret private_key entre 2 et prime_number - 1 inclus avec random.randint(min, max)
     # 2. Calcule la clef publique public_key = generator ** private_key % prime_number; utiliser la fonction pow_mod
     # 3. Renvoit le dictionnaitre {"public_key": A, "private_key": a}
-    pass
+    a = random.randint(2, prime_number - 1)
+    A = pow(generator, a, prime_number)
+    return {"public_key": A, "private_key": a}
 
 
 def compute_shared_secret_key(public: int, private: int, prime_number: int) -> int:
     # Implementer le calcul de la clef secrete partagée à partir de la clef publique de l'autre participant et de ma clef privée
     # Utiliser pow_mod
+    return pow_mod(public, private, prime_number)
     pass
--- src/cryptoy/rsa_cipher.py
+++ src/cryptoy/rsa_cipher.py
@@ -15,22 +15,30 @@ def keygen() -> dict:
     e = 65537
     # Implementez la génération de clef de RSA avec e = 65537
     # 1. Tire aléatoirement un nombre premier p avec la fonction draw_random_prime
+    p = draw_random_prime()
     # 2. Tire aléatoirement un nombre premier q avec la fonction draw_random_prime
+    q = draw_random_prime()
     # 3. Calcul de d, l'inverse de e modulo (p - 1) * (q - 1), avec la fonction modular_inverse
+    d = modular_inverse(e, (p - 1) * (q - 1))
     # 4. Renvoit un dictionnaire { "public_key": (e, p * q), "private_key": d}
-    pass
+    return {"public_key": (e, p * q), "private_key": d}
 
 
 def encrypt(msg: str, public_key: tuple) -> int:
     # Implementez le chiffrement rsa d'un message avec une clef publique de la forme (e, N)
     # 1. Convertir le message en nombre entier avec la fonction str_to_int
+    enc = str_to_int(msg)
     # 2. Verifiez que ce nombre est < public_key[1], sinon lancer une exception
+    if enc >= public_key[1]:
+        raise ValueError("Message is too long for the key")
     # 3. Chiffrez le nombre entier avec pow_mod et les paramètre de la clef publique (e, N)
-    pass
+    return pow_mod(enc, public_key[0], public_key[1])
 
 
 def decrypt(msg: int, key: dict) -> str:
     # Implementez le dechiffrement rsa d'un message avec une clef de la forme { "public_key": (e, p * q), "private_key": d}
     # 1. Utilisez pow_mod avec les paramètres de la clef
+    msg = pow_mod(msg, key["private_key"], key["public_key"][1])
+
     # 2. Convertir l'entier calculé en str avec la fonction int_to_str
-    pass
+    return str(int_to_str(msg)) 
